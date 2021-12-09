2852e9b81710d1453823629258d3a975bd0acf7a
Scorcherfjk <franjavikale@gmail.com>
2021-12-09 16:04:56 -0500

    feat: integrate components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
-const Discord = require('discord.js');
-const config = require('./config');
+const Discord = require("discord.js");
+
+const config = require("./config");
+const DiscordController = require("./controllers/discord.controller");
+const discordController = new DiscordController();
 
 const client = new Discord.Client();
 client.login(config.token);
 
-
-client.on('ready', () => {
+client.on("ready", () => {
   console.log(`Conected like ${client.user.tag}`);
 });
 
-client.on('message', async (msg) => {
-  
+client.on("message", async (msg) => {
   if (msg.author.bot) return;
-  console.log('He recibido el mensaje ', msg.channel.type)
 
   if (msg.channel.type === 'dm') {
-    if (msg.content.toLocaleLowerCase() === 'hola') {
-      msg.reply('Hola');
+  // if (msg.content.toLocaleLowerCase() === "hola") {
+    const { has_reaction, reaction, message } = await discordController.receive(
+      msg
+    );
+
+    if (has_reaction) {
+      msg.react(reaction);
     }
+    msg.reply(message);
+  // }
   }
 });
